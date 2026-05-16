f037cdd0e73ba780e29d59a30abebb4d57af390c
Dineth <dinethlewkebandara@gmail.com>
2026-05-16 17:44:34 +0530

    committing Project

--- src/main/java/com/bakenest/Controller/CustomerController.java
+++ src/main/java/com/bakenest/Controller/CustomerController.java
@@ -22,6 +22,7 @@ import java.util.Optional;
 //Creates controller class named CustomerController.
 public class CustomerController {
 
+    //Spring automatically injects CustomerService.
     @Autowired
     private CustomerService customerService;
 
