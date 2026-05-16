c6cda0cc1346082826b5de75d5dc302e82a76fd1
RANDIL <kavindurandil60@gmail.com>
2026-05-17 01:07:35 +0530

    Commit files

---
+++ src/main/java/com/bakenest/Repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.bakenest.Repository;
+
+import com.bakenest.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Custom query to find products by category for the filter buttons
+    List<Product> findByCategory(String category);
+    boolean existsByName(String name);
+    boolean existsByImageUrl(String imageUrl);
+}
